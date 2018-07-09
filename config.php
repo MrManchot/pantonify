69bd132e96dc13bd7ffd19e778731ae996f49cfe
Mouleyre Cédric <mouleyrecedric@gmail.com>
2018-07-09 23:14:18 +0200

    Suite

--- config.php
+++ config.php
@@ -1,12 +1,8 @@
 <?php
 
 // SpreadShirt
-// Redirection www
 // Bouton print + Nouvel import
-// Limite uplaod serveur
-// Favicon
 // Hauteur des blocks, resize...
-// Google analytics
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Pantonify</title>
+    <link rel="icon" type="image/png" href="favicon.png" />
     <link rel="stylesheet" href="dist/css/pantonify.min.css" />
 </head>
 
@@ -11,6 +12,14 @@
     <form action="ajax.php" class="dropzone" id="pantonify-dropzone"></form>
     <div id="pantonify-container" class="container"></div>
     <script src="dist/js/pantonify.min.js"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122025561-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-122025561-1');
+    </script>
+
 </body>
 
 </html>
